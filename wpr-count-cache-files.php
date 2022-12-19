07a0cd3d81d881018f412285cbab7107cd1ce090
Alfonso Catron <alfonso.catron@gmail.com>
2022-12-19 12:26:56 -0300

    Create wpr-count-cache-files.php

---
+++ wpr-count-cache-files.php
@@ -0,0 +1,17 @@
+<?php
+
+$d = date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
+$s = $_SERVER['SERVER_NAME'];	
+
+$dir = new RecursiveDirectoryIterator( "wp-content/cache/wp-rocket/{$s}/");
+$flat = new RecursiveIteratorIterator($dir);
+
+$files = new RegexIterator($flat, '/^.+\.html_gzip$/i');
+$filecount = count(iterator_to_array($files));
+
+
+echo "<pre>[{$d}] Found {$filecount} .html_gzip cache files for {$s}:\n\n";
+foreach($files as $file) {
+        echo $file . "\n";
+}
+echo "</pre>";
